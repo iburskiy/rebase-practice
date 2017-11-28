16affab82b84ef1a8bffae9f34f2593dbde0f1e9
iburskyy <iburskyy@exadel.com>
2017-11-28 17:59:46 +0300

    local-cleanup commit1

--- router.js
+++ router.js
@@ -1,3 +1,5 @@
+// local-cleanup commit1
+
 function route(handle, pathname, response, request) {
   console.log("About to route a request for " + pathname);
   if (typeof handle[pathname] === 'function') {
